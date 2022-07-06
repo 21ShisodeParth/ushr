141259b0936fdefe46c3ff7c8a6c1871b7ea531b
akshayraj3000 <47067194+akshayraj3000@users.noreply.github.com>
2022-07-05 19:39:12 -0500

    Add files via upload

---
+++ ushr.py
@@ -0,0 +1,51 @@
+import ticketpy
+import tekore as tk
+import requests
+
+sp = tk.Spotify()
+token = 'BQAqwZpnGMlIkFMz7c4kK_KOMCZBAYu0knyJxEV-mmNtpTHh4U1D9UNzJOThG50yLPrbbsvKsFiQTdTdk71GzfEfVhAWATac3J4Eyx46AGgGT1AdOV874l68X8jjYQloRf38ynB9GVcvx9DYNT8RlQSnByQdvO7J26n45biRRPWPXOWNb6h9jpWmo1KHXvak1R4HCzNJRUKli2EsWz62l7Masp2EXkBXnhHkvJTxXB1KM11Tm-sbhDp_hAqurA117buPPa-QLrwIkZxskCE5ls13Cfg6YNlvSrLy_A'
+
+# artists = []
+# with sp.token_as(token):
+#     for term in ["long_term", "short_term"]:
+#         temp_artists = sp.current_user_top_artists(limit=1, offset=0, time_range=term)
+#         for artist in temp_artists.items:
+#             artists.append(artist.name)
+
+# print(artists)
+
+key = 'nsLfwpueMjA6sRNDrJ8QOg90LOO4ApAj'
+tm = ticketpy.ApiClient(key)
+artists = ["Kanye West", "BIGBABYGUCCI"]
+
+def get_event(ticket_url, event_id):
+    event = requests.get(ticket_url,  json = {"id":event_id})
+    json_resp = event.json()
+    return json_resp
+
+events = []
+for artist in artists:
+    temp = tm.events.find(
+        classification_name='Hip-Hop',
+        keyword=artist
+    )
+
+    for pages in temp:
+        for event in pages:
+            event_id = event.id
+            ticket_url = 'https://app.ticketmaster.com/discovery/v2/events/' + str(event_id) + '.json?apikey=nsLfwpueMjA6sRNDrJ8QOg90LOO4ApAj'
+            temp2 = [
+                artist,
+                get_event(ticket_url, event_id)['name'],
+                get_event(ticket_url, event_id)['url'],
+                get_event(ticket_url, event_id)['id'],
+                get_event(ticket_url, event_id)['locale'],
+                get_event(ticket_url, event_id)['dates']['start']['localDate'],
+                get_event(ticket_url, event_id)['_embedded']['venues'][0]['name'],
+                get_event(ticket_url, event_id)['_embedded']['venues'][0]['postalCode']
+                ]
+
+            events.append(temp2)
+
+for event in events:
+    print(event)
