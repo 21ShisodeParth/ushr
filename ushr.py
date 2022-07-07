28e7962fd3bb106c8d3bf2ae48610b9f516124e0
21ShisodeParth <52021668+21ShisodeParth@users.noreply.github.com>
2022-07-07 12:50:08 -0700

    Update ushr.py

--- ushr.py
+++ ushr.py
@@ -2,50 +2,81 @@ import ticketpy
 import tekore as tk
 import requests
 
+# Input your own Spotify API token.
+token = ''
 sp = tk.Spotify()
-token = '' #your token
 
+# Input your own Ticketmaster key.
+key = ''
+tm = ticketpy.ApiClient(key)
+
+# Collecting and compounding the user's top 50 artists for short-term listening and long-term listening.
+'''
 artists = []
 with sp.token_as(token):
     for term in ["long_term", "short_term"]:
-        temp_artists = sp.current_user_top_artists(limit=1, offset=0, time_range=term)
+        temp_artists = sp.current_user_top_artists(limit = 1, offset = 0, time_range = term)
         for artist in temp_artists.items:
-            artists.append(artist.name)
+            # Ensuring no repetitions in the 'artists' list will exist.
+            if artist.name not in artists:
+                artists.append(artist.name)
 
-# print(artists)
+print(artists) 
+'''
 
-key = '' #your token
-tm = ticketpy.ApiClient(key)
-artists = ["Kanye West", "BIGBABYGUCCI"]
+# //just example artists
+artists = ["Kanye West", "BIGBABYGUCCI", "Gregory Alan Isakov"]
 
+
+# Function to handle the JSON extraction of event information.
 def get_event(ticket_url, event_id):
     event = requests.get(ticket_url,  json = {"id":event_id})
     json_resp = event.json()
     return json_resp
 
-events = []
-for artist in artists:
-    temp = tm.events.find(
-        classification_name='Hip-Hop',
-        keyword=artist
-    )
+def gather(element):
+    try:
+        return element
+    except:
+        return None
 
-    for pages in temp:
-        for event in pages:
-            event_id = event.id
-            ticket_url = 'https://app.ticketmaster.com/discovery/v2/events/' + str(event_id) + '.json?apikey=nsLfwpueMjA6sRNDrJ8QOg90LOO4ApAj'
-            temp2 = [
-                artist,
-                get_event(ticket_url, event_id)['name'],
-                get_event(ticket_url, event_id)['url'],
-                get_event(ticket_url, event_id)['id'],
-                get_event(ticket_url, event_id)['locale'],
-                get_event(ticket_url, event_id)['dates']['start']['localDate'],
-                get_event(ticket_url, event_id)['_embedded']['venues'][0]['name'],
-                get_event(ticket_url, event_id)['_embedded']['venues'][0]['postalCode']
-                ]
+def handle_events(sort_by = None, genre = None):
 
-            events.append(temp2)
+    events = []
+    for artist in artists:
+        temp = tm.events.find(
+            # FIND A WAY TO HAVE ALL GENRES IF GENRE IS NONE
+            classification_name = genre,
+            keyword=artist
+        )
 
-for event in events:
-    print(event)
+        for pages in temp:
+            for event in pages:
+                event_id = event.id
+                ticket_url = 'https://app.ticketmaster.com/discovery/v2/events/' + str(event_id) + '.json?apikey=' + key
+
+                date_info = get_event(ticket_url, event_id).get('dates')
+                emb_info = get_event(ticket_url, event_id).get('_embedded')
+
+                event_dict = {
+                    'name' : get_event(ticket_url, event_id).get('name'),
+                    'url' : get_event(ticket_url, event_id).get('url'),
+                    'id' : get_event(ticket_url, event_id).get('id'),
+                    'locale' : get_event(ticket_url, event_id).get('locale'),
+                    'localDate' : date_info['start']['localDate'] if date_info else 'n/a',
+                    'name' : emb_info['venues'][0]['name'] if emb_info else 'n/a',
+                    'postalCode' : emb_info['venues'][0]['postalCode'] if emb_info else 'n/a'
+                }
+
+                events.append((artist, event_dict))
+    
+
+    match sort_by:
+        case 'localDate':
+            events = sorted(events, key = lambda x: x[1]['localDate'])
+        # ADD MORE SORTING CASES
+
+    for event in events:
+        print(event)
+
+handle_events('localDate')
